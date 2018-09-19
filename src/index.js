e8fe947c48005013eaecbee05a3e6e3042ab54c5
GoldenKit <nikitoska01@gmail.com>
2018-09-19 22:36:35 +0300

    sollution_love-triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let res = 0;
+  let pr = preferences;
+   for (let i = 0; i < pr.length; i++) {
+     if (pr[i] - 1 == i) continue;
+     let ab = pr[i];
+     let bc = pr[ab-1];
+     let ca = pr[bc-1];
+     if (ca - 1 == i){
+       res++
+       pr[i]=0
+       pr[ab-1]=0
+       pr[bc-1]=0;
+     }
+   }
+ return res;
 };
